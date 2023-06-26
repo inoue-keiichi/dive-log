8f3eba7342eb301bec3b38c1e044782009ffaa77
inoue-keiichi <inouejai@gmail.com>
2023-06-27 00:31:53 +0900

    deleteボタン作成

--- __tests__/components/templates/DiveLogForm.test.tsx
+++ __tests__/components/templates/DiveLogForm.test.tsx
@@ -7,6 +7,7 @@ import {
   fillTransparency,
   fillWaterTemprature,
 } from "../__utils__/diveLogForm";
+import renderer from "react-test-renderer";
 
 const submit = jest.fn();
 const back = jest.fn();
@@ -14,6 +15,8 @@ const deletee = jest.fn(); // 予約語
 
 afterEach(() => {
   submit.mockClear();
+  back.mockClear();
+  deletee.mockClear();
 });
 
 describe("test", () => {
@@ -27,9 +30,9 @@ describe("test", () => {
     const target = screen.getByText("追加");
     await fireEvent.click(target);
 
-    // react-hook-form によって submit が呼び出されるまで待機
+    // react-hook-form によって呼び出されるまで待機
     await waitFor(() => expect(submit).toHaveBeenCalledTimes(1));
-    await waitFor(() => expect(back).toHaveBeenCalledTimes(0));
+    expect(back).toHaveBeenCalledTimes(0);
     expect(submit.mock.calls[0][0]).toStrictEqual({
       date: "2022-04-01",
       point: "Ose",
@@ -42,9 +45,9 @@ describe("test", () => {
     render(<DiveLogForm onSubmit={submit} onBack={back} />);
     const target = screen.getByText("追加");
     await fireEvent.click(target);
-    // react-hook-form によって submit が呼び出されるまで待機
+    // react-hook-form によって呼び出されるまで待機
     await waitFor(() => expect(submit).toHaveBeenCalledTimes(1));
-    await waitFor(() => expect(back).toHaveBeenCalledTimes(0));
+    expect(back).toHaveBeenCalledTimes(0);
     expect(submit.mock.calls[0][0]).toStrictEqual({
       date: getSimpleDate(new Date()),
       point: "",
@@ -55,8 +58,7 @@ describe("test", () => {
 
   test("succeeded in submitting form with default values", async () => {
     const diveLog = {
-      id: 1,
-      userId: "uuid",
+      date: "2023-06-27",
       point: "Ose",
       waterTemprature: 28,
       transparency: 8,
@@ -65,9 +67,9 @@ describe("test", () => {
     render(<DiveLogForm diveLog={diveLog} onSubmit={submit} onBack={back} />);
     const target = screen.getByText("上書き");
     await fireEvent.click(target);
-    // react-hook-form によって submit が呼び出されるまで待機
+    // react-hook-form によって呼び出されるまで待機
     await waitFor(() => expect(submit).toHaveBeenCalledTimes(1));
-    await waitFor(() => expect(back).toHaveBeenCalledTimes(0));
+    expect(back).toHaveBeenCalledTimes(0);
   });
 
   test("succeeded in going back page", async () => {
@@ -76,20 +78,57 @@ describe("test", () => {
     const target = screen.getByTestId("back-button");
     await fireEvent.click(target);
 
-    // react-hook-form によって submit が呼び出されるまで待機
+    // react-hook-form によって呼び出されるまで待機
     await waitFor(() => expect(submit).toHaveBeenCalledTimes(0));
-    await waitFor(() => expect(back).toHaveBeenCalledTimes(1));
+    expect(back).toHaveBeenCalledTimes(1);
   });
 
   test("succeeded in delete button", async () => {
-    render(<DiveLogForm onSubmit={submit} onBack={back} onDelete={deletee} />);
+    const diveLog = {
+      date: "2023-06-27",
+      point: "Ose",
+      waterTemprature: 28,
+      transparency: 8,
+    };
+
+    render(
+      <DiveLogForm
+        diveLog={diveLog}
+        onSubmit={submit}
+        onBack={back}
+        onDelete={deletee}
+      />
+    );
 
     const target = screen.getByTestId("dive-log-delete-button");
     await fireEvent.click(target);
 
-    // react-hook-form によって submit が呼び出されるまで待機
+    // react-hook-form によって呼び出されるまで待機
     await waitFor(() => expect(submit).toHaveBeenCalledTimes(0));
-    await waitFor(() => expect(back).toHaveBeenCalledTimes(0));
-    await waitFor(() => expect(back).toHaveBeenCalledTimes(1));
+    expect(back).toHaveBeenCalledTimes(0);
+    expect(deletee).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("snapshot test", () => {
+  test("new Dive log form", async () => {
+    const tree = renderer
+      .create(<DiveLogForm onSubmit={submit} onBack={back} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("existing Dive log form", async () => {
+    const diveLog = {
+      date: "2023-06-27",
+      point: "Ose",
+      waterTemprature: 28,
+      transparency: 8,
+    };
+
+    const tree = renderer
+      .create(<DiveLogForm diveLog={diveLog} onSubmit={submit} onBack={back} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
