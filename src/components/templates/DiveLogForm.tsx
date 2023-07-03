a10c6b0e6b3e73d727187f8fe03d0c358d99803e
inoue-keiichi <inouejai@gmail.com>
2023-07-03 23:21:29 +0900

    ログブックの項目追加

--- src/components/templates/DiveLogForm.tsx
+++ src/components/templates/DiveLogForm.tsx
@@ -9,14 +9,29 @@ import {
   OutlinedInput,
   IconButton,
   Grid,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
 } from "@mui/material";
 import { FC } from "react";
 import { FieldErrors, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { getSimpleDate } from "@/utils/commons";
 import DeleteIcon from "@mui/icons-material/Delete";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import WbCloudyOutlinedIcon from "@mui/icons-material/WbCloudyOutlined";
+import {
+  WiDaySunny,
+  WiCloud,
+  WiDayCloudy,
+  WiRain,
+  WiSnow,
+} from "react-icons/wi";
+import { BsFillSunFill } from "react-icons/bs";
+
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import { grey } from "@mui/material/colors";
+import { blue, grey, orange, red } from "@mui/material/colors";
 
 type Props = {
   diveLog?: DiveLog;
@@ -80,6 +95,20 @@ const DiveLogForm: FC<Props> = (props) => {
         </FormHelperText>
       </FormControl>
       <FormControl>
+        <InputLabel htmlFor="place">場所</InputLabel>
+        <OutlinedInput
+          id="place"
+          label="場所"
+          type="text"
+          error={!!errors.place?.message}
+          defaultValue={diveLog?.place}
+          {...register("place")}
+        />
+        <FormHelperText error={!!errors.point?.message}>
+          {errors.point?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
         <InputLabel htmlFor="point">ポイント</InputLabel>
         <OutlinedInput
           id="point"
@@ -94,6 +123,226 @@ const DiveLogForm: FC<Props> = (props) => {
         </FormHelperText>
       </FormControl>
       <FormControl>
+        <InputLabel htmlFor="divingStartTime">潜水開始時間</InputLabel>
+        <OutlinedInput
+          id="divingStartTime"
+          label="潜水開始時間"
+          type="time"
+          error={!!errors.divingStartTime?.message}
+          // typeがdateの場合inputのvalue形式はyyyy-mm-ddになる
+          defaultValue={diveLog?.divingStartTime || "00:00"}
+          {...register("divingStartTime")}
+        />
+        <FormHelperText error={!!errors.divingStartTime?.message}>
+          {errors.divingStartTime?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="divingEndTime">潜水終了時間</InputLabel>
+        <OutlinedInput
+          id="divingEndTime"
+          label="潜水終了時間"
+          type="time"
+          error={!!errors.divingEndTime?.message}
+          // typeがdateの場合inputのvalue形式はyyyy-mm-ddになる
+          defaultValue={diveLog?.divingEndTime || "00:00"}
+          {...register("divingEndTime")}
+        />
+        <FormHelperText error={!!errors.divingStartTime?.message}>
+          {errors.divingEndTime?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="averageDepth">平均水深</InputLabel>
+        <OutlinedInput
+          id="averageDepth"
+          label="平均水深"
+          type="number"
+          error={!!errors.averageDepth?.message}
+          defaultValue={diveLog?.averageDepth}
+          endAdornment={<InputAdornment position="end">m</InputAdornment>}
+          {...register("averageDepth")}
+        />
+        <FormHelperText error={true}>
+          {errors.averageDepth?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="maxDepth">最大水深</InputLabel>
+        <OutlinedInput
+          id="maxDepth"
+          label="最大水深"
+          type="number"
+          error={!!errors.maxDepth?.message}
+          defaultValue={diveLog?.maxDepth}
+          endAdornment={
+            <InputAdornment position="end">
+              {"\u{338F}/\u{33A0}"}
+            </InputAdornment> // kg/cm^2
+          }
+          {...register("maxDepth")}
+        />
+        <FormHelperText error={true}>
+          {errors.maxDepth?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="tankStartPressure">タンク圧力(開始)</InputLabel>
+        <OutlinedInput
+          id="tankStartPressure"
+          label="タンク圧力(開始)"
+          type="number"
+          error={!!errors.tankStartPressure?.message}
+          defaultValue={diveLog?.tankStartPressure}
+          endAdornment={<InputAdornment position="end">m</InputAdornment>}
+          {...register("tankStartPressure")}
+        />
+        <FormHelperText error={true}>
+          {errors.tankStartPressure?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="tankEndPressure">タンク圧力(終了)</InputLabel>
+        <OutlinedInput
+          id="tankEndPressure"
+          label="タンク圧力(終了)"
+          type="number"
+          error={!!errors.tankEndPressure?.message}
+          defaultValue={diveLog?.tankEndPressure}
+          endAdornment={<InputAdornment position="end">m</InputAdornment>}
+          {...register("tankEndPressure")}
+        />
+        <FormHelperText error={true}>
+          {errors.tankEndPressure?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <FormLabel id="tankKind">タンク</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="tankKind"
+          defaultValue="steel"
+          name="tank-kinds-group"
+        >
+          <FormControlLabel
+            value="steel"
+            control={<Radio />}
+            label="スチール"
+            {...register("tankKind")}
+          />
+          <FormControlLabel
+            value="aluminum"
+            control={<Radio />}
+            label="アルミニウム"
+            {...register("tankKind")}
+          />
+        </RadioGroup>
+        <FormHelperText error={true}>
+          {errors.tankKind?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <FormLabel id="suit">スーツ</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="suit"
+          defaultValue="wet"
+          name="wet-kinds-group"
+        >
+          <FormControlLabel
+            value="wet"
+            control={<Radio />}
+            label="ウェット"
+            {...register("suit")}
+          />
+          <FormControlLabel
+            value="dry"
+            control={<Radio />}
+            label="ドライ"
+            {...register("suit")}
+          />
+        </RadioGroup>
+        <FormHelperText error={true}>
+          {errors.suit?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="weight">ウェイト</InputLabel>
+        <OutlinedInput
+          id="weight"
+          label="ウェイト"
+          type="number"
+          error={!!errors.weight?.message}
+          defaultValue={diveLog?.weight}
+          endAdornment={
+            <InputAdornment position="end">{"\u{338F}"}</InputAdornment>
+          }
+          {...register("weight")}
+        />
+        <FormHelperText error={true}>
+          {errors.weight?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <FormLabel id="weather">天気</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="weather"
+          defaultValue="sunny"
+          name="wet-weathers-group"
+        >
+          <Radio
+            value="sunny"
+            icon={<WiDaySunny size="2em" />}
+            checkedIcon={<WiDaySunny size="2em" />}
+            {...register("weather")}
+          />
+          <Radio
+            value="sunny/cloudy"
+            icon={<WiDayCloudy size="2em" />}
+            checkedIcon={<WiDayCloudy size="2em" />}
+          />
+          <Radio
+            value="cloudy"
+            icon={<WiCloud size="2em" />}
+            checkedIcon={<WiCloud size="2em" />}
+            {...register("weather")}
+          />
+          <Radio
+            value="rainy"
+            icon={<WiRain size="2em" />}
+            checkedIcon={<WiRain size="2em" />}
+            {...register("weather")}
+          />
+          <Radio
+            value="snowy"
+            icon={<WiSnow size="2em" />}
+            checkedIcon={<WiSnow size="2em" />}
+            {...register("weather")}
+          />
+        </RadioGroup>
+        <FormHelperText error={true}>
+          {errors.weather?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="temprature">気温</InputLabel>
+        <OutlinedInput
+          id="temprature"
+          label="気温"
+          type="number"
+          error={!!errors.temprature?.message}
+          defaultValue={diveLog?.temprature}
+          endAdornment={
+            <InputAdornment position="end">{"\u{2103}"}</InputAdornment> // ℃
+          }
+          {...register("temprature")}
+        />
+        <FormHelperText error={true}>
+          {errors.temprature?.message ?? ""}
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
         <InputLabel htmlFor="waterTemprature">水温</InputLabel>
         <OutlinedInput
           id="waterTemprature"
@@ -101,7 +350,9 @@ const DiveLogForm: FC<Props> = (props) => {
           type="number"
           error={!!errors.waterTemprature?.message}
           defaultValue={diveLog?.waterTemprature}
-          endAdornment={<InputAdornment position="end">℃</InputAdornment>}
+          endAdornment={
+            <InputAdornment position="end">{"\u{2103}"}</InputAdornment> // ℃
+          }
           {...register("waterTemprature")}
         />
         <FormHelperText error={true}>
--- src/schemas/diveLog.ts
+++ src/schemas/diveLog.ts
@@ -1,37 +1,88 @@
 import { z } from "zod";
 
-export const diveLogSchema = z.object({
-  date: z
+const text = (between: { max: number }) => {
+  const { max } = between;
+
+  return z
     .string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/, {
-      message: "日付のフォーマットを YYYY-MM-DD にしてください",
-    })
-    .refine((value) => !isNaN(new Date(value).getTime()), {
-      message: "存在しない日付です",
-    }),
-  point: z
-    .string()
-    .max(63, { message: "63文字以下にしてください" })
+    .min(0)
+    .max(max, { message: `${max}文字以下にしてください` })
     .nullable()
-    .optional(),
-  waterTemprature: z.preprocess((v) => {
-    if (!v || v === "") {
-      return null;
-    }
-    if (typeof Number(v) === "number") {
-      return Number(v);
-    }
-    return v;
-  }, z.number().max(100, { message: "100以下の数字にしてください" }).min(-100, { message: "-100以上の数字にしてください" }).nullish()),
-  transparency: z.preprocess((v) => {
-    if (!v || v === "") {
-      return null;
-    }
-    if (typeof Number(v) === "number") {
-      return Number(v);
-    }
-    return v;
-  }, z.number().max(100, { message: "100以下の数字にしてください" }).min(0, { message: "0以上の数字にしてください" }).nullish()),
+    .nullish();
+};
+
+const time = z
+  .string()
+  .regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, {
+    message: "日付のフォーマットを HH:mm にしてください",
+  })
+  .nullish();
+
+const date = z
+  .string()
+  .regex(/^\d{4}-\d{2}-\d{2}$/, {
+    message: "日付のフォーマットを YYYY-MM-DD にしてください",
+  })
+  .refine((value) => !isNaN(new Date(value).getTime()), {
+    message: "存在しない日付です",
+  });
+
+const temprature = z.preprocess((v) => {
+  if (!v || v === "") {
+    return null;
+  }
+  if (typeof Number(v) === "number") {
+    return Number(v);
+  }
+  return v;
+}, z.number().max(100, { message: "100以下の数字にしてください" }).min(-100, { message: "-100以上の数字にしてください" }).nullish());
+
+const number = (between: { max: number; min: number }) => {
+  const { max, min } = between;
+  return z.preprocess(
+    (v) => {
+      if (!v || v === "") {
+        return null;
+      }
+      if (typeof Number(v) === "number") {
+        return Number(v);
+      }
+      return v;
+    },
+    z
+      .number()
+      .max(max, { message: `${max}以下の数字にしてください` })
+      .min(min, { message: `${min}以上の数字にしてください` })
+      .nullish()
+  );
+};
+
+export const diveLogSchema = z.object({
+  date,
+  place: text({ max: 63 }),
+  point: text({ max: 63 }),
+  divingStartTime: time,
+  divingEndTime: time,
+  averageDepth: number({ min: 0, max: 100 }),
+  maxDepth: number({ min: 0, max: 100 }),
+  tankStartPressure: number({ min: 0, max: 500 }),
+  tankEndPressure: number({ min: 0, max: 500 }),
+  tankKind: z.union([z.literal("steel"), z.literal("aluminum")]).nullish(),
+  suit: z.union([z.literal("wet"), z.literal("dry")]).nullish(),
+  weight: number({ min: 0, max: 50 }),
+  weather: z
+    .union([
+      z.literal("sunny"),
+      z.literal("sunny/cloudy"),
+      z.literal("cloudy"),
+      z.literal("rainy"),
+      z.literal("snowy"),
+    ])
+    .nullish(),
+  temprature,
+  waterTemprature: temprature,
+  transparency: number({ min: 0, max: 100 }),
+  comment: text({ max: 511 }),
 });
 
 export const newDiveLogQuerySchema = z.object({ userId: z.string() });
