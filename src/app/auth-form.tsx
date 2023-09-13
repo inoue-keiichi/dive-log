7340a82cc3cef7edcc7f67ee09449e8b01aa79d9
inoue-keiichi <inouejai@gmail.com>
2023-09-14 01:18:17 +0900

    wip

--- src/app/auth-form.tsx
+++ src/app/auth-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { supabaseClient } from "@/clients/supabase";
-import styles from "@/styles/Home.module.css";
 import { SITE_URL } from "@/utils/commons";
 import { Stack } from "@mui/material";
 import { Auth } from "@supabase/auth-ui-react";
@@ -8,16 +7,14 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default function AuthForm() {
   return (
-    <main className={styles.main}>
-      <Stack>
-        <Auth
-          providers={[]}
-          redirectTo={`${SITE_URL}/auth/callback`}
-          appearance={{ theme: ThemeSupa }}
-          supabaseClient={supabaseClient}
-          socialLayout="horizontal"
-        />
-      </Stack>
-    </main>
+    <Stack>
+      <Auth
+        providers={[]}
+        redirectTo={`${SITE_URL}/auth/callback`}
+        appearance={{ theme: ThemeSupa }}
+        supabaseClient={supabaseClient}
+        socialLayout="horizontal"
+      />
+    </Stack>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { supabaseClient } from "@/clients/supabase";
 import AuthForm from "./auth-form";
 
 export default function Home() {
-  supabaseClient.auth.getUser();
-
   return <AuthForm />;
 }
